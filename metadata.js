0e765605ab33a7638bcd83592b7f0136542e9013
SilasReinagel <silas.reinagel@gmail.com>
2022-04-12 23:22:45 -0700

    Updated Metadata

--- metadata.js
+++ metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Weisbrod Design',
-  url: 'www.weisbroddesign.com',
-  image: '',
-  description: 'Homepage of Arizona Artist Joshua Weisbrod, Acclaimed Artist and Designer',
+  url: 'https://www.weisbroddesign.com',
+  image: 'https://weisbroddesign-prototype.netlify.app/images/card.jpg',
+  description: 'Acclaimed Graphic Designer Joshua Weisbrod',
   keywords: 'graphic design,hgtv art'
 };
